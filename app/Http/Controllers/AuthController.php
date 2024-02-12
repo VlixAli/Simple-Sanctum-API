ada5da451f4e4de862feb9b0ec8a7ffd4194573d
Ali <qali372@gmail.com>
2024-02-12 10:27:37 +0200

    feat : implement logout in AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,6 +53,10 @@ class AuthController extends Controller
 
     public function logout()
     {
-        return response()->json('this is my logout method');
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success([
+            'message' => 'You have successfully been logged out and your token has been deleted'
+        ]);
     }
 }
