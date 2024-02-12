15ad66b7b7a778ffb58990340617f30b48831eda
Ali <qali372@gmail.com>
2024-02-12 08:24:31 +0200

    feat : implement login in AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,19 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
 use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     use HttpResponses;
 
-    public function login()
+    public function login(Request $request)
     {
-        return 'This is my login method';
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+
+        if(!Auth::attempt($formFields)){
+            return $this->error('' , 'Credentials do not match' , 401);
+        }
+
+        $user = User::where('email' , $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'toke' => $user->createToken('Api token of ' . $user->name)->plainTextToken
+        ]);
     }
 
     public function register(StoreUserRequest $request)
@@ -24,8 +41,9 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->passowrd)
+            'password' => $request->password
         ]);
+
 
         return $this->success([
             'user' => $user ,
